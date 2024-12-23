ad4be23995b5f4f6a6fbd8c62f3df6404049299b
Mauricio <105090929+MauAlvesM@users.noreply.github.com>
2024-12-23 19:46:19 +0000

    arrumando a frase inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Mauricio, tu é brabo demais </h1>
+    return <h1>O prazer e a dor, e os que os produzem, o bem e o mal, são os eixos em que assentam todas as nossas paixões. </h1>
     
     
 }
