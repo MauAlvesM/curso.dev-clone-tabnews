22970baf6f871ef37cfb4916cc316d4044f585b4
Mauricio <105090929+MauAlvesM@users.noreply.github.com>
2025-01-08 01:03:06 +0000

    Mensagem modficiada

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 function Home() {
   return (
     <h1>
-      O prazer e a dor, e os que os produzem, o bem e o mal, são os eixos em que
-      assentam todas as nossas paixões.{" "}
+      "A vida é como um jogo de xadrez: sempre existe uma próxima jogada.”
+      Fallout.{" "}
     </h1>
   );
 }
